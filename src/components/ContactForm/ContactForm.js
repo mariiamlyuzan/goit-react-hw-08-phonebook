562d6122445ef8af23bdb0910acbf9bb27283edb
mariiamlyuzan <86401047+mariiamlyuzan@users.noreply.github.com>
2022-07-18 19:48:59 +0300

    add i18n

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -97,7 +97,7 @@ export default function ContactForm() {
               ariaLabel="loading"
             />
           ) : (
-            t('addcontact')
+            t('addContact')
           )}
         </button>
       </form>
